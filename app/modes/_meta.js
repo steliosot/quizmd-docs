7861b091b6c8ae4196a887ad2a275627e96e8bc7
Stelios Sotiriadis <steliosot@msn.com>
2026-04-24 21:12:26 +0100

    Document room modes and AI provider support

--- app/modes/_meta.js
+++ app/modes/_meta.js
@@ -2,5 +2,8 @@ export default {
   index: 'Modes Overview',
   classic: 'Classic Mode',
   imposter: 'Imposter Mode',
-  essay: 'Essay Mode'
+  essay: 'Essay Mode',
+  compete: 'Room: Compete',
+  collaborate: 'Room: Collaborate',
+  boxing: 'Room: Boxing'
 }
